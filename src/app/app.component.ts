149b7d038ab96e10bc9a3b7f4eaf2ea9f185e3bd
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-05-14 14:58:33 -0700

    new project idea, instagram clone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,9 @@ export class AppComponent {
     new Beers(`pepis`, `https://i.pinimg.com/originals/33/c1/50/33c150f7fca6cad8f8f4239d7378fd50.png`, `this stuff is good owo`)
   ];
   submitForm(newTitle, newPhoto, newDescription){
-    this.currentBeers.unshift(new Beers(newTitle, newPhoto, newDescription))
+    const newPost = new Beers(newTitle, newPhoto, newDescription)
+    this.currentBeers.unshift(newPost)
   }
-
   deleteTask(currentBeers){
     var index = this.currentBeers.indexOf(currentBeers);
     this.currentBeers.splice(index, 1);
