27a079a3d4a404fcfbb4cef61576eceedb5b7844
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-05-14 11:38:57 -0700

    add delete item function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,14 @@ import { Beers } from './models/beers';
 export class AppComponent {
   title = 'app';
   currentBeers: Beers[] = [
-    new Beers('newWorld')
+    new Beers(`pepis`, `https://i.pinimg.com/originals/33/c1/50/33c150f7fca6cad8f8f4239d7378fd50.png`, `this stuff is good owo`)
   ];
-  submitForm(newWorld: Beers){
-    this.currentBeers.unshift(new Beers(newWorld))
-    console.log(newWorld)
+  submitForm(newTitle, newPhoto, newDescription){
+    this.currentBeers.unshift(new Beers(newTitle, newPhoto, newDescription))
+  }
+
+  deleteTask(currentBeers){
+    var index = this.currentBeers.indexOf(currentBeers);
+    this.currentBeers.splice(index, 1);
   }
 }
