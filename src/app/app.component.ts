f209a14ccb8e4df68d899b4d336f1a328c3d1fbb
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-05-15 13:12:14 -0700

    add navbar to the page and rename it to Tutor-hub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,26 @@ import { NewsFeed } from './models/beers';
 })
 export class AppComponent {
   title = 'app';
+  changeAbout = null;
   currentFeed: NewsFeed[] = [
-    new NewsFeed(`Olan Rodger`, `https://previews.123rf.com/images/vadymvdrobot/vadymvdrobot1509/vadymvdrobot150900446/45025475-closeup-portrait-of-a-handsome-man-at-gym.jpg`, `programmer`),
-    new NewsFeed(`Emily Anderson`, `http://www.slate.com/content/dam/slate/blogs/xx_factor/2014/susan.jpg.CROP.promo-mediumlarge.jpg`, `math tutor`),
-    new NewsFeed(`Neil Parson`, `https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=350`, `Science tutor`),
-    new NewsFeed(`Jasim Gonzalez`, `http://www.ifred.org/wp-content/uploads/2014/11/woman.jpg`, `Science tutor`),
-    new NewsFeed(`Alex Rax`, `https://i.kinja-img.com/gawker-media/image/upload/s--_DBGLHVf--/c_scale,f_auto,fl_progressive,q_80,w_800/eibgv7kctah62iddzywm.jpg`, `History Tutor`)
+    new NewsFeed(`Olan Rodger`, `https://previews.123rf.com/images/vadymvdrobot/vadymvdrobot1509/vadymvdrobot150900446/45025475-closeup-portrait-of-a-handsome-man-at-gym.jpg`, `Computer Science`),
+    new NewsFeed(`Emily Anderson`, `http://www.slate.com/content/dam/slate/blogs/xx_factor/2014/susan.jpg.CROP.promo-mediumlarge.jpg`, `math`),
+    new NewsFeed(`Neil Parson`, `https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=350`, `Science`),
+    new NewsFeed(`Jasim Gonzalez`, `http://www.ifred.org/wp-content/uploads/2014/11/woman.jpg`, `Science`),
+    new NewsFeed(`Alex Rax`, `https://i.kinja-img.com/gawker-media/image/upload/s--_DBGLHVf--/c_scale,f_auto,fl_progressive,q_80,w_800/eibgv7kctah62iddzywm.jpg`, `History`)
   ];
-  submitForm(newTitle, newPhoto, newDescription){
+  submitForm(newTitle, newPhoto, newDescription) {
     const newPost = new NewsFeed(newTitle, newPhoto, newDescription)
     this.currentFeed.unshift(newPost)
   }
-  deleteTask(currentFeed){
+  deleteTask(currentFeed) {
     var index = this.currentFeed.indexOf(currentFeed);
     this.currentFeed.splice(index, 1);
+  }
+  editProfile(currentFeed) {
+    this.changeAbout = currentFeed;
+  }
+  doneEdit() {
+    this.changeAbout = null;
   }
 }
